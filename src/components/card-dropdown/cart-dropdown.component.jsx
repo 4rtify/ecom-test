e0519d98ccf4e481a6918d12597a07639620454a
Nikolay Miroshnichenko <nikolai.miroshnichenko@gmail.com>
2022-06-13 15:50:43 +1000

    shopping checkout

--- src/components/card-dropdown/cart-dropdown.component.jsx
+++ src/components/card-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { ShoppingCartContext } from '../../contexts/shopping-cart.context';
 
@@ -9,15 +10,20 @@ import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
   const {cartItems} = useContext(ShoppingCartContext);
+  const navigate = useNavigate();
 
+  const goToCheckoutHandler = () => {
+    navigate('/checkout')
+  }
+  
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
         {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item}/>
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   )
 }
